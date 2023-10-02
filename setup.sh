e2da37b9bf24333a7937c00f683c36bd972550b3
vaijuv <104854904+vaijuv@users.noreply.github.com>
2023-10-02 17:09:41 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-
+export DATABASE_PATH="postgresql://capstone_zjmd_user:OYmQWm5dos48HBaIF9bSweU2vAT1qE0y@dpg-ckd7b2tjhfbs73dn1580-a.oregon-postgres.render.com/capstone_zjmd" #postgresql://postgres:Welcome2023@localhost:5432/capstone"
+export AUTH0_DOMAIN='capstonefsnd.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='capstone'
